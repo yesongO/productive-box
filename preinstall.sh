092252f42f03b52448b9ba271920334d903e67d1
oh ye-song <yesong9874@naver.com>
2025-07-17 00:59:36 +0900

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Check if CI variable is set to true
-if [ "$CI" = "true" ]; then
-  echo "CI is true, skipping pre-commit install."
-else
+#if [ "$CI" = "true" ]; then
+  #echo "CI is true, skipping pre-commit install."
+#else
   # Install pre-commit hooks
   # pre-commit install
-fi
+#fi
